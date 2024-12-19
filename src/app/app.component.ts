1d11bbe9e33b9e4bcb097f7eb588bf74fc5c2184
Khoi Bui <khoi.bui@accesso.com>
2024-12-19 07:38:55 -0500

    day 3 part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ const COMMA = 6;
 const D2 = 7;
 const CP = 8;
 
+const D = 9;
+const O = 10;
+const N = 11;
+const SQ = 12;
+const T = 13;
+const D_OP = 14;
+const D_CP = 15;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,7 +27,7 @@ const CP = 8;
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+  input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
   result = '';
 
   onSubmit() {
@@ -28,6 +36,7 @@ export class AppComponent {
     let prevStage: number = ZERO;
     let number1 = '',
       number2 = '';
+    let start = -1;
     for (let i = 0; i < str.length; i++) {
       const stage = this.checkStage(prevStage, str[i]);
       if ((prevStage === OP || prevStage === D1) && stage === D1) {
@@ -37,14 +46,84 @@ export class AppComponent {
         number2 += str[i];
       }
       if (prevStage === D2 && stage === CP) {
-        res += +number1 * +number2;
+        const action = this.findAction(str.slice(0, start));
+        if (!!action) {
+          res += +number1 * +number2;
+        }
       }
       if (stage === ZERO || stage === M) {
         (number1 = ''), (number2 = '');
       }
+      if (stage === M) {
+        start = i;
+      }
       prevStage = stage;
     }
     this.result = `${res}`;
+  }
+
+  findAction(str: string): boolean | null {
+    let prevStage = 0;
+    let action = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      const stage = this.checkDoStage(prevStage, str[i]);
+      if (stage >= D && stage <= D_CP) {
+        action = `${str[i]}${action}`;
+      }
+      if (prevStage !== ZERO && stage === D) {
+        return !action.includes("don't()");
+      }
+      if (stage === ZERO || stage === D) {
+        action = '';
+      }
+      prevStage = stage;
+    }
+    return true;
+  }
+
+  checkDoStage(stage: number, char: string): number {
+    switch (stage) {
+      case ZERO:
+        if (char === ')') {
+          return D_CP;
+        }
+        return 0;
+      case D_CP:
+        if (char === '(') {
+          return D_OP;
+        }
+        return 0;
+      case D_OP:
+        if (char === 't') {
+          return T;
+        }
+        if (char === 'o') {
+          return O;
+        }
+        return 0;
+      case T:
+        if (char === "'") {
+          return SQ;
+        }
+        return 0;
+      case SQ:
+        if (char === 'n') {
+          return N;
+        }
+        return 0;
+      case N:
+        if (char === 'o') {
+          return O;
+        }
+        return 0;
+      case O:
+        if (char === 'd') {
+          return D;
+        }
+        return 0;
+      default:
+        return 0;
+    }
   }
 
   checkStage(stage: number, char: string): number {
