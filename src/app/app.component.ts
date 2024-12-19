0b5eec29550c412d5144a7859502c6e933e0297a
Khoi Bui <khoi.bui@accesso.com>
2024-12-19 13:58:30 -0500

    day 6 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+const UP = '^';
+const DOWN = 'v';
+const LEFT = '<';
+const RIGHT = '>';
+const OBSTACLE = '#';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,101 +15,131 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`;
+  input = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`;
   result = '';
-  ruleMap!: Map<any, any[]>;
+  steps = new Set();
 
   onSubmit() {
-    const data = this.parseRow(this.input);
-    const splitPt = data.findIndex((item) => !item.trim());
-    this.ruleMap = this.storeRules(data.slice(0, splitPt));
-    const updates = data.slice(splitPt + 1).map((item) => item.split(','));
-    let total = 0;
-    for (let row = 0; row < updates.length; row++) {
-      const passed = this.testRow(updates[row]);
-      if (!passed) {
-        const sortedArr = this.sort(updates[row]);
-        total += +this.getMiddle(sortedArr);
-      }
-    }
-
-    this.result = `${total}`;
-  }
-
-  swapElements(arr: any[], x: number, y: number) {
-    const b = arr[y];
-    arr[y] = arr[x];
-    arr[x] = b;
-    return arr;
-  }
-
-  isGreater(num1: number, num2: any): boolean {
-    const rules = this.ruleMap.get(num2);
-    return !!rules?.includes(num1);
-  }
-
-  testRow(updates: any[]): boolean {
-    for (let col = 0; col < updates.length; col++) {
-      for (let i = col + 1; i < updates.length; i++) {
-        if (this.isGreater(updates[col], updates[i])) {
-          return false;
+    const map = this.parseRow(this.input).map((item) => item.split(''));
+    let currPos = this.findStartingPos(map);
+    if (currPos) {
+      this.steps.clear();
+      this.addToSteps(currPos);
+      while (!this.isOutOfMap(map, currPos)) {
+        if (this.hasObstacle(map, currPos)) {
+          currPos = { ...currPos, dir: this.changeDir(currPos.dir) };
+        } else {
+          currPos = { ...this.move(currPos) };
+          this.addToSteps(currPos)
         }
       }
     }
-    return true;
+    this.result = `${this.steps.size}`;
   }
 
-  sort(arr: any[]): any[] {
-    let updates = [...arr];
-    for (let col = 0; col < updates.length; col++) {
-      for (let i = col + 1; i < updates.length; i++) {
-        if (this.isGreater(updates[col], updates[i])) {
-          updates = this.swapElements(updates, col, i);
+  move(currPos: { dir: string; row: number; col: number }): {
+    dir: string;
+    row: number;
+    col: number;
+  } {
+    switch (currPos.dir) {
+      case UP:
+        return { ...currPos, row: currPos.row - 1 };
+      case RIGHT:
+        return { ...currPos, col: currPos.col + 1 };
+      case DOWN:
+        return { ...currPos, row: currPos.row + 1 };
+      case LEFT:
+        return { ...currPos, col: currPos.col - 1 };
+      default:
+        return currPos;
+    }
+  }
+
+  changeDir(currDir: string): string {
+    switch (currDir) {
+      case UP:
+        return RIGHT;
+      case RIGHT:
+        return DOWN;
+      case DOWN:
+        return LEFT;
+      case LEFT:
+        return UP;
+      default:
+        return currDir;
+    }
+  }
+
+  hasObstacle(
+    map: any[][],
+    currPos: { dir: string; row: number; col: number }
+  ): boolean {
+    switch (currPos.dir) {
+      case UP:
+        return map[currPos.row - 1][currPos.col] === OBSTACLE;
+      case DOWN:
+        return map[currPos.row + 1][currPos.col] === OBSTACLE;
+      case LEFT:
+        return map[currPos.row][currPos.col - 1] === OBSTACLE;
+      case RIGHT:
+        return map[currPos.row][currPos.col + 1] === OBSTACLE;
+      default:
+        return false;
+    }
+  }
+
+  isOutOfMap(
+    map: any[][],
+    currPos: { dir: string; row: number; col: number }
+  ): boolean {
+    const { width, height } = this.mapSize(map);
+
+    switch (currPos.dir) {
+      case UP:
+        return currPos.row - 1 < 0;
+      case DOWN:
+        return currPos.row + 1 === height;
+      case LEFT:
+        return currPos.col - 1 < 0;
+      case RIGHT:
+        return currPos.col + 1 === width;
+      default:
+        return false;
+    }
+  }
+
+  mapSize(map: any[][]) {
+    return {
+      width: map[0].length,
+      height: map.length,
+    };
+  }
+
+  addToSteps(currPos: { row: number; col: number }) {
+    this.steps.add(`${currPos.row}-${currPos.col}`);
+  }
+
+  findStartingPos(
+    map: any[][]
+  ): { dir: string; row: number; col: number } | null {
+    for (let row = 0; row < map.length; row++) {
+      for (let col = 0; col < map[row].length; col++) {
+        if ([UP, DOWN, LEFT, RIGHT].includes(map[row][col])) {
+          return { dir: map[row][col], row, col };
         }
       }
     }
-    return updates;
-  }
-
-  getMiddle(arr: any[]) {
-    return arr[(arr.length / 2) | 0];
-  }
-
-  storeRules(arr: any[]): Map<any, any[]> {
-    const map = new Map();
-    arr.forEach((item) => {
-      const [num1, num2] = item.split('|');
-      const values = map.get(num1);
-      values?.length ? map.set(num1, [...values, num2]) : map.set(num1, [num2]);
-    });
-    return map;
+    return null;
   }
 
   parseRow(data: any): any[] {
