98910138cf5e5d89967b3f2572ee776af4ffa7b2
Khoi Bui <khoi.bui@accesso.com>
2024-12-30 18:48:14 -0500

    day 12 part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ const UP = 0;
 const DOWN = 1;
 const LEFT = 2;
 const RIGHT = 3;
-const MAX_SIDE = 4;
 
 /**
  * Use Depth First Search
@@ -18,11 +17,11 @@ const MAX_SIDE = 4;
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  //   input = `AAAA
-  // BBCD
-  // BBCC
-  // EEEC`;
-  input = `RRRRIICCFF
+//   input = `AAAA
+// BBCD
+// BBCC
+// EEEC`;
+    input = `RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
 VVRCCCJFFF
@@ -33,7 +32,7 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE`;
   result = signal('');
-  visited = new Set();
+  visitedMap = new Set();
 
   onSubmit() {
     const map = this.parseRow(this.input).map((element) => element.split(''));
@@ -42,7 +41,7 @@ MMMISSJEEE`;
     this.result.set(`...waiting`);
 
     setTimeout(() => {
-      this.visited.clear();
+      this.visitedMap.clear();
       const total = this.start(map);
       this.result.set(`${total}`);
     }, 0);
@@ -53,75 +52,122 @@ MMMISSJEEE`;
     for (let row = 0; row < map.length; row++) {
       for (let col = 0; col < map[row].length; col++) {
         if (!this.hasVisited({ row, col })) {
-          const { area, fences } = this.searchArea(
+          const region: any[] = [];
+          const { area } = this.searchArea(
             map,
             { row, col },
-            map[row][col]
+            map[row][col],
+            region
           );
-          total += area * fences;
+          total += area * this.countCorners(region);
         }
       }
     }
     return total;
   }
 
+  countCorners(region: any[]): number {
+    let count = 0;
+
+    const arr = region
+      .slice()
+      .sort((a, b) => (a.row === a.row ? a.col - b.col : a.row - b.row));
+    const locSet = new Set(arr.map((item) => this.formatLoc(item)));
+
+    arr.forEach((item) => {
+      const { row, col } = item;
+
+      // top-left
+      if (
+        !locSet.has(this.formatLoc({ row, col: col - 1 })) &&
+        !locSet.has(this.formatLoc({ row: row - 1, col }))
+      )
+        count++;
+
+      // top-left-diagonal
+      if (
+        locSet.has(this.formatLoc({ row, col: col - 1 })) &&
+        locSet.has(this.formatLoc({ row: row - 1, col })) &&
+        !locSet.has(this.formatLoc({ row: row - 1, col: col - 1 }))
+      )
+        count++;
+
+      // top-right
+      if (
+        !locSet.has(this.formatLoc({ row, col: col + 1 })) &&
+        !locSet.has(this.formatLoc({ row: row - 1, col }))
+      )
+        count++;
+
+      // top-right-diagonal
+      if (
+        locSet.has(this.formatLoc({ row, col: col + 1 })) &&
+        locSet.has(this.formatLoc({ row: row - 1, col })) &&
+        !locSet.has(this.formatLoc({ row: row - 1, col: col + 1 }))
+      )
+        count++;
+
+      // bottom-left
+      if (
+        !locSet.has(this.formatLoc({ row, col: col - 1 })) &&
+        !locSet.has(this.formatLoc({ row: row + 1, col }))
+      )
+        count++;
+
+      // bottom-left-diagonal
+      if (
+        locSet.has(this.formatLoc({ row, col: col - 1 })) &&
+        locSet.has(this.formatLoc({ row: row + 1, col })) &&
+        !locSet.has(this.formatLoc({ row: row + 1, col: col - 1 }))
+      )
+        count++;
+
+      // bottom-right
+      if (
+        !locSet.has(this.formatLoc({ row, col: col + 1 })) &&
+        !locSet.has(this.formatLoc({ row: row + 1, col }))
+      )
+        count++;
+
+      // bottom-right-diagonal
+      if (
+        locSet.has(this.formatLoc({ row, col: col + 1 })) &&
+        locSet.has(this.formatLoc({ row: row + 1, col })) &&
+        !locSet.has(this.formatLoc({ row: row + 1, col: col + 1 }))
+      )
+        count++;
+    });
+    return count;
+  }
+
   searchArea(
     map: any[][],
-    curr: { row: number; col: number },
-    type: string
-  ): { area: number; fences: number } {
-    let totalArea = 0,
-      totalFences = 0;
+    curr: { row: number; col: number; dir?: number },
+    type: string,
+    region: any[]
+  ): { area: number } {
+    let totalArea = 0;
     const nextMoves = this.getNextMoves(map, curr, type);
     if (nextMoves?.length) {
-      this.visited.add(this.formatLoc(curr));
+      this.visitedMap.add(this.formatLoc(curr));
+      region.push(curr);
       nextMoves.forEach((move) => {
-        const { area, fences } = this.searchArea(map, move, type);
-        totalArea += area;
-        totalFences += fences;
+        if (!this.hasVisited(move)) {
+          const { area } = this.searchArea(map, move, type, region);
+          totalArea += area;
+        }
       });
       return {
         area: 1 + totalArea,
-        fences: totalFences + this.countEdges(map, curr, type),
       };
     } else {
       if (!this.hasVisited(curr)) {
-        this.visited.add(this.formatLoc(curr));
-        return { area: 1, fences: this.countEdges(map, curr, type) };
+        this.visitedMap.add(this.formatLoc(curr));
+        region.push(curr);
+        return { area: 1 };
       }
-      return { area: 0, fences: 0 };
+      return { area: 0 };
     }
-  }
-
-  countEdges(
-    map: any[][],
-    curr: { row: number; col: number },
-    type: string
-  ): number {
-    let count = 0;
-    let neighbor = null;
-    const width = map[0].length;
-    const height = map.length;
-
-    // right
-    neighbor = { row: curr.row, col: curr.col + 1 };
-    if (neighbor.col >= width || map[neighbor.row][neighbor.col] !== type)
-      count++;
-
-    // up
-    neighbor = { row: curr.row - 1, col: curr.col };
-    if (curr.row - 1 < 0 || map[neighbor.row][neighbor.col] !== type) count++;
-
-    // down
-    neighbor = { row: curr.row + 1, col: curr.col };
-    if (curr.row + 1 >= height || map[neighbor.row][neighbor.col] !== type)
-      count++;
-
-    // left
-    neighbor = { row: curr.row, col: curr.col - 1 };
-    if (curr.col - 1 < 0 || map[neighbor.row][neighbor.col] !== type) count++;
-
-    return count;
   }
 
   getNextMoves(
@@ -159,7 +205,7 @@ MMMISSJEEE`;
       neighbor.row < height &&
       map[neighbor.row][neighbor.col] === type
     ) {
-      arr.push({ row: curr.row + 1, col: curr.col, dir: DOWN });
+      arr.push({ ...neighbor, col: curr.col, dir: DOWN });
     }
     // left
     neighbor = { row: curr.row, col: curr.col - 1 };
@@ -174,7 +220,7 @@ MMMISSJEEE`;
   }
 
   hasVisited(curr: { row: number; col: number }) {
-    return this.visited.has(this.formatLoc(curr));
+    return this.visitedMap.has(this.formatLoc(curr));
   }
 
   formatLoc(curr: { row: number; col: number }): string {
