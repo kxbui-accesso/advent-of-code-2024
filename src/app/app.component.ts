a4fff8c8b9aba8a6002f4f1800540ae70e0080b5
Khoi Bui <khoi.bui@accesso.com>
2025-01-09 20:38:46 -0500

    day 21 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-const TIME_SAVED_THRESHOLD = 50;
+const ROBOT_NUM = 2;
 
+const NUMERIC_KEYPAD = `7 8 9
+4 5 6
+1 2 3
+# 0 A`;
+
+const DIRECTIONAL_KEYPAD = `# ^ A
+< v >`;
+
+const UP = '^';
+const DOWN = 'v';
+const LEFT = '<';
+const RIGHT = '>';
 const WALL = '#';
-const SAFE = '.';
-const START = 'S';
-const END = 'E';
-const MAX_CHEAT = 20;
+const ACTIVATE = 'A';
 
-/**
- * Implement Breadth First Search
- */
+interface Node {
+  row: number;
+  col: number;
+  parent?: Node;
+  direction: string;
+  count?: number;
+}
+
+type SearchResult = Node;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,219 +36,247 @@ const MAX_CHEAT = 20;
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############`;
+  input = `029A
+980A
+179A
+456A
+379A`;
 
   result = signal('');
+  pathMap = new Map<string, SearchResult[]>();
 
   onSubmit() {
     this.result.set(`...waiting`);
 
     setTimeout(() => {
-      const input = this.parseInput(this.parseRow(this.input));
+      const input = this.parseRow(this.input);
       const total = this.start(input);
       this.result.set(`${total}`);
     }, 0);
   }
 
-  start(map: any[][]): number {
-    const pathMap = this.reconstructPath(this.search(map));
-    return this.findCheats(pathMap);
-  }
-
-  search(map: any[][]): { row: number; col: number; parent: any } | null {
-    const visited = new Set();
-    const queue: any[] = [];
-    const start = this.findPosition(map, START);
-
-    if (start) {
-      visited.add(this.formatLoc(start));
-      queue.push(start);
-
-      while (queue.length) {
-        const s = queue.shift();
-        if (s) {
-          if (this.isGoal(map, s)) {
-            return s;
-          }
-          const paths = this.findPaths(map, s);
-          paths
-            .filter((path) => !visited.has(this.formatLoc(path)))
-            .forEach((path) => {
-              const node = { ...path, parent: s };
-              visited.add(this.formatLoc(path));
-              queue.push(node);
-            });
-        }
-      }
-    }
-    return null;
-  }
-
-  /**
-   * HINT: During the execution of a cheat,
-   * the program can enter and leave walls multiple times,
-   * as long as the cheat ends in normal track, within the maximal allowed cheat time
-   * of 20 picoseconds.
-   * @param map
-   * @param startNode
-   * @returns
-   */
-  findCheats(pathMap: Map<string, number>): number {
+  start(lines: string[]): number {
     let total = 0;
-    const arr = Array.from(pathMap.keys());
+    this.pathMap.clear();
 
-    for (let x = 0; x < arr.length - 1; x++) {
-      for (let y = x + 1; y < arr.length; y++) {
-        const [startRow, startCol] = arr[x].split('-');
-        const [endRow, endCol] = arr[y].split('-');
-        const count = this.calcDistance(
-          { row: Number(startRow), col: Number(startCol) },
-          { row: Number(endRow), col: Number(endCol) }
-        );
-        if (count) {
-          const timeSaved = y - x - count;
-          if (timeSaved === TIME_SAVED_THRESHOLD) {
-            total++;
-          }
-        }
-      }
-    }
+    lines.forEach((line) => {
+      const sequence = this.getSequence(line);
+      total += this.getCodeComplexity(line, sequence);
+    });
     return total;
   }
 
-  /**
-   * Use Manhattan distance
-   * @param startNode
-   * @param endNode
-   */
-  calcDistance(
-    startNode: { row: number; col: number },
-    endNode: { row: number; col: number }
-  ): number | null {
-    const dist =
-      Math.abs(startNode.row - endNode.row) +
-      Math.abs(startNode.col - endNode.col);
+  getSequence(line: string): string {
+    let arr = this.searchKeypad(line, false);
+    let robots = ROBOT_NUM;
 
-    return dist <= MAX_CHEAT ? dist : null;
+    while (robots > 0) {
+      let tempArr: any[] = [];
+      arr.forEach((seq) => {
+        const result = this.searchKeypad(seq);
+        tempArr = [...tempArr, ...result];
+      });
+      arr = this.findShortestStrings(tempArr);
+      robots--;
+    }
+
+    return arr[0];
   }
 
-  canPassThrWall(
+  findShortestStrings(arr: any[]) {
+    if (arr.length === 0) {
+      return [];
+    }
+
+    let minLength = arr[0].length;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].length < minLength) {
+        minLength = arr[i].length;
+      }
+    }
+
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length === minLength) {
+        result.push(arr[i]);
+      }
+    }
+
+    return result;
+  }
+
+  getCodeComplexity(code: string, sequence: string) {
+    return this.getDigit(code) * sequence.length;
+  }
+
+  searchKeypad(line: string, directional = true): string[] {
+    let start = directional ? { row: 0, col: 2 } : { row: 3, col: 2 };
+    const map = this.parseRow(
+      directional ? DIRECTIONAL_KEYPAD : NUMERIC_KEYPAD
+    ).map((line) => line.trim().split(/\s*[\s,]\s*/));
+    let arr: any[] = [];
+
+    for (let i = 0; i < line.length; i++) {
+      const goal = line[i];
+      const sequences = this.getPathFromKeypad(map, start, goal);
+      if (sequences) {
+        arr = this.generatePermutations(
+          arr,
+          sequences.map(this.reconstructPath)
+        );
+        start = { row: sequences[0].row, col: sequences[0].col };
+      }
+    }
+
+    return arr;
+  }
+
+  generatePermutations(arr1: any[], arr2: any[]): any[] {
+    const arr: any[] = [];
+
+    if (!arr1.length && !arr2.length) return arr;
+    if (!arr1.length) return arr2;
+    if (!arr2.length) return arr1;
+
+    for (let x = 0; x < arr1.length; x++) {
+      for (let y = 0; y < arr2.length; y++) {
+        arr.push(`${arr1[x]}${arr2[y]}`);
+      }
+    }
+    return arr;
+  }
+
+  getPathFromKeypad(
     map: any[][],
-    node1: { row: number; col: number },
-    node2: { row: number; col: number }
-  ): { row: number; col: number } | null {
-    if (
-      this.validNode(map, node1) &&
-      this.isWall(map, node1) &&
-      this.validNode(map, node2) &&
-      this.canMove(map, node2)
-    )
-      return node2;
+    start: { row: number; col: number },
+    goal: string
+  ): SearchResult[] {
+    if (map[start.row][start.col] === goal)
+      return [{ ...start, direction: '' }];
 
-    return null;
+    const key = `${map[start.row][start.col]}-${goal}`;
+    if (this.pathMap.has(key)) {
+      return this.pathMap.get(key)!;
+    }
+    const result = this.search(map, start, goal);
+    this.pathMap.set(key, result);
+    return result;
   }
 
-  isSameNode(
-    node1: { row: number; col: number },
-    node2: { row: number; col: number }
-  ): boolean {
-    return node1.row === node2.row && node1.col === node2.col;
+  search(
+    map: any[][],
+    start: { row: number; col: number },
+    goal: string
+  ): SearchResult[] {
+    const visited = new Set();
+    const queue: any[] = [];
+    const arr: any[] = [];
+    let minLength = Infinity;
+
+    queue.push({ ...start, count: 0 });
+
+    while (queue.length) {
+      const indexToRemove = this.findSmallestIdx(queue);
+      const [s] = queue.splice(indexToRemove, 1);
+
+      if (s) {
+        if (this.isGoal(map, s, goal)) {
+          const length = this.reconstructPath(s).length;
+          if (minLength >= length) {
+            arr.push(s);
+            minLength = length;
+          }
+        } else {
+          if (s.count <= minLength) {
+            const paths = this.findPaths(map, s);
+            paths.forEach((path) => {
+              const node = { ...path, parent: s, count: s.count + 1 };
+              visited.add(this.formatLoc(path));
+              queue.push(node);
+            });
+          }
+        }
+      }
+      visited.add(this.formatLoc(start));
+    }
+    return arr;
   }
 
-  validNode(map: any[][], node: { row: number; col: number }) {
-    const width = map[0].length;
-    const height = map.length;
-
-    return (
-      node.row >= 0 && node.row < height && node.col >= 0 && node.col < width
-    );
+  findSmallestIdx(arr: any[]) {
+    if (arr.length === 1) {
+      return 0;
+    }
+    let smallest = arr[0].f;
+    let idx = 0;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].f < smallest) {
+        smallest = arr[i].count;
+        idx = i;
+      }
+    }
+    return idx;
   }
 
-  reconstructPath(
-    node: { parent: any; row: number; col: number } | null
-  ): Map<string, number> {
-    const map = new Map();
-    if (!node) return map;
+  reconstructPath(node: SearchResult): string {
+    if (!node) return '';
 
-    let curr = node,
-      path = [];
+    let path = ACTIVATE;
+    let curr: Node | undefined = node;
 
     while (curr) {
-      path.push({ row: curr.row, col: curr.col });
+      path = curr.direction ? `${curr.direction}${path}` : path;
       curr = curr.parent;
     }
 
-    path.reverse();
-    path.forEach((node, i) => map.set(this.formatLoc(node), i));
-
-    return map;
+    return path;
   }
 
   findPaths(map: any[][], curr: { row: number; col: number }): any[] {
     let node = null;
     const arr: any[] = [];
 
-    node = { row: curr.row - 1, col: curr.col };
-    if (this.validNode(map, curr) && this.canMove(map, node)) arr.push(node);
+    node = { row: curr.row - 1, col: curr.col, direction: UP };
+    if (this.validNode(map, node)) arr.push(node);
 
-    node = { row: curr.row + 1, col: curr.col };
-    if (this.validNode(map, curr) && this.canMove(map, node)) arr.push(node);
+    node = { row: curr.row + 1, col: curr.col, direction: DOWN };
+    if (this.validNode(map, node)) arr.push(node);
 
-    node = { row: curr.row, col: curr.col - 1 };
-    if (this.validNode(map, curr) && this.canMove(map, node)) arr.push(node);
+    node = { row: curr.row, col: curr.col - 1, direction: LEFT };
+    if (this.validNode(map, node)) arr.push(node);
 
-    node = { row: curr.row, col: curr.col + 1 };
-    if (this.validNode(map, curr) && this.canMove(map, node)) arr.push(node);
+    node = { row: curr.row, col: curr.col + 1, direction: RIGHT };
+    if (this.validNode(map, node)) arr.push(node);
 
     return arr;
   }
 
-  canMove(map: any[][], curr: { row: number; col: number }) {
-    return [SAFE, END].includes(map[curr.row][curr.col]);
+  getDigit(str: string): number {
+    return Number(str.match(/\d+/)?.[0]);
   }
 
-  isGoal(map: any[][], curr: { row: number; col: number }): boolean {
-    return map[curr.row][curr.col] === END;
+  validNode(map: any[][], node: Node) {
+    const width = map[0].length;
+    const height = map.length;
+
+    return (
+      node.row >= 0 &&
+      node.row < height &&
+      node.col >= 0 &&
+      node.col < width &&
+      map[node.row][node.col] !== WALL
+    );
   }
 
-  isWall(map: any[][], curr: { row: number; col: number }): boolean {
-    return map[curr.row][curr.col] === WALL;
+  isGoal(
+    map: any[][],
+    curr: { row: number; col: number },
+    goal: string
+  ): boolean {
+    return map[curr.row][curr.col] === goal;
   }
 
   formatLoc(currPos: { row: number; col: number }) {
     return [currPos.row, currPos.col].join('-');
-  }
-
-  findPosition(
-    map: any[][],
-    type: string
-  ): { row: number; col: number } | null {
-    for (let row = 0; row < map.length; row++) {
-      for (let col = 0; col < map[row].length; col++) {
-        if (map[row][col] === type) {
-          return { row, col };
-        }
-      }
-    }
-    return null;
-  }
-
-  parseInput(input: string[]): any[][] {
-    return input.map((line) => line.split(''));
   }
 
   parseRow(data: any): any[] {
